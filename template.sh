dd6470906a6245daf123a69f10cd3c7eb729c707
Baptist Levrel <levrel.baptist@protonmail.com>
2022-12-17 00:39:59 +0100

    done

--- template.sh
+++ template.sh
@@ -11,14 +11,14 @@ echo "{" >> $FILE_NAME
 echo "" >> $FILE_NAME
 echo "private:" >> $FILE_NAME
 echo "" >> $FILE_NAME
-echo "	//private attributes here" >> $FILE_NAME
+echo "	//private attributes here;" >> $FILE_NAME
 echo "" >> $FILE_NAME
 echo "public:" >> $FILE_NAME
 echo "" >> $FILE_NAME
-echo "	"${CLASS_NAME^}"(void);" >> $FILE_NAME
-echo "	"${CLASS_NAME^}"(const ${CLASS_NAME^} & other);" >> $FILE_NAME
-echo "	"~${CLASS_NAME^}"(void);" >> $FILE_NAME
-echo "	"${CLASS_NAME} "operator=(const ${CLASS_NAME^} & other);" >> $FILE_NAME
+echo "	${CLASS_NAME^}(void);" >> $FILE_NAME
+echo "	${CLASS_NAME^}(const ${CLASS_NAME^} & other);" >> $FILE_NAME
+echo "	~${CLASS_NAME^}(void);" >> $FILE_NAME
+echo "	${CLASS_NAME^} & operator=(const ${CLASS_NAME^} & other);" >> $FILE_NAME
 echo "" >> $FILE_NAME
 echo "};" >> $FILE_NAME
 echo "" >> $FILE_NAME
@@ -41,9 +41,9 @@ echo "{" >> $FILE_NAME
 echo "	std::cout << \"Default destructor called\" << std::endl;" >> $FILE_NAME
 echo "}" >> $FILE_NAME
 echo "" >> $FILE_NAME
-echo "${CLASS_NAME^}::operator=(const ${CLASS_NAME^} & other)" >> $FILE_NAME
+echo "${CLASS_NAME^} & ${CLASS_NAME^}::operator=(const ${CLASS_NAME^} & other)" >> $FILE_NAME
 echo "{" >> $FILE_NAME
-echo "	//copy operations here" >> $FILE_NAME
+echo "	//copy operations here;" >> $FILE_NAME
 echo "	std::cout << \"Assignment operator called\" << std::endl;" >> $FILE_NAME
 echo "	return (*this);" >> $FILE_NAME
 echo "}" >> $FILE_NAME
